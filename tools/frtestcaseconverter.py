40acdb4ab799ec43e90da7fb3021a9e114afe28c
pguo <patrick.pguo@gmail.com>
2024-10-01 15:38:47 -0400

    Implement template rendering for class structure

--- tools/frtestcaseconverter.py
+++ tools/frtestcaseconverter.py
@@ -1,38 +1,69 @@
 import os
 from tests.frparser.frtestparserv4 import V4FrTestParser, TestCommands
+from jinja2 import Environment, FileSystemLoader
 
 READ_TEST_DIR = '../tests/testcases'
 WRITE_TEST_DIR = '../tests/testfrtestcases'
 
+CLASS_TEMPLATE = './fr_testcase_class_template.txt'
+FUNC_TEMPLATE = './fr_testcase_function_template.txt'
+
 class FrTCConverter:
-    def __init__(self):
+    def __init__(self, read_dir, write_dir):
         self.parser = V4FrTestParser()
         self.testcases = []
+
+        self.read_dir = read_dir
+        self.write_dir = write_dir
+
+        class_template_env = Environment(loader=FileSystemLoader(''))
+        function_template_env = Environment(loader=FileSystemLoader(''))
+
+        self.class_template = class_template_env.get_template(CLASS_TEMPLATE)
+        self.function_template = function_template_env.get_template(FUNC_TEMPLATE)
 
     def parse_file(self, filename):
         self.testcases = []
 
         looking_for_match = False
         with open(filename, 'r') as fileopen:
-            for line in fileopen:
-                cmd, testcase = self.parser.start(line)
+            for line_num, line_content in enumerate(fileopen):
+                cmd, testcase = self.parser.start(line_content)
                 match cmd:
                     case TestCommands.encode_pair.value | TestCommands.decode_pair.value:
                         if not looking_for_match:
-                            self.testcases.append(testcase)
+                            self.testcases.append((line_num, cmd, testcase))
                             looking_for_match = True
                     case TestCommands.match.value:
                         if looking_for_match:
-                            self.testcases.append(testcase)
+                            self.testcases.append((line_num, cmd, testcase))
                             looking_for_match = False
 
-    def write_to_file(self, filename):
+    def write_to_file(self, filedir, filename: str):
         # don't create an "empty" unittest file
         if not self.testcases:
             return
 
-        with open(filename, 'w+') as fileopen:
-            fileopen.write()
+        fullpath = os.path.join(
+            filedir,
+            f"test{filename.replace('.txt', '.py')}"
+        )
+        classname = f'Test{filename.split('.')[0].title()}'
+
+        with open(fullpath, 'w') as fileopen:
+            fileopen.write(
+                self.class_template.render(
+                    class_name = classname,
+                )
+            )
+
+            for testcase in self.testcases:
+                fileopen.write(
+                    self.function_template.render(
+                        testcase_name = f'{testcase[0]}',
+                        testcase = f'self.assertTrue(True)'
+                    )
+                )
 
     def mirror_file_structure(self, read_dir, write_dir):
         for filename in os.listdir(read_dir):
@@ -43,8 +74,11 @@ class FrTCConverter:
                                       os.path.join(write_dir, filename))
             elif os.path.isfile(os.path.join(read_dir, filename)):
                 self.parse_file(os.path.join(read_dir, filename))
-                self.write_to_file(os.path.join(write_dir, filename))
+                self.write_to_file(write_dir, filename)
+
+    def start(self):
+        self.mirror_file_structure(self.read_dir, self.write_dir)
 
 if __name__ == '__main__':
-    converter = FrTCConverter()
-    converter.mirror_file_structure(READ_TEST_DIR, WRITE_TEST_DIR)
+    converter = FrTCConverter(READ_TEST_DIR, WRITE_TEST_DIR)
+    converter.start()
